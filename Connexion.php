b0fd283c72cbed71de7c1862923878cb30300550
Nagatip <78030029+Nagatip@users.noreply.github.com>
2021-01-26 14:52:58 +0100

    Add files via upload

---
+++ Connexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang='fr'>
+	<head>
+		<title> Application Web MECO </title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="Connexion.css"/>
+	</head>
+	<body>
+			<table>
+				<tbody>
+					<tr> <td> <img src="MECO image.png"/> </td> </tr>
+					<tr> <td> <label> Utilisateur: </label> </td> </tr>
+					<tr> <td> <input  type="text" name="nom" value="" size="20" maxlength="20"/> </td> </tr>
+					<tr> <td> <label> Mot de Passe: </label> </td> </tr>
+					<tr> <td> <input type="password" name="mot_de_passe" value="" size="20" maxlength="20"/> </td> </tr>
+					<tr> <td> <a href='http://projetbts/Equipement.php'> <input type="button" value="Connexion" /> </a> </td> </tr>
+				</tbody>
+			</table>
+	</body>
+</html>
---
+++ Equipement.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang='fr'>
+	<head>
+		<title> Application Web MECO </title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" />
+	</head>
+	<body>
+		<button onclick=" "> Ajouter à un conteneur </button>
+		<button onclick=" "> Information sur l'équipement </button>
+		<button onclick=" "> Deconnexion </button>
+	</body>
+</html>
